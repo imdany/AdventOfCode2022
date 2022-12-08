18c04ffabf934af956ec747d7134e502a7703371
imdany <dangzpz@gmail.com>
2022-12-08 13:00:17 +0100

    Day 7

--- src/Day7/Day7.ts
+++ src/Day7/Day7.ts
@@ -1,6 +1,68 @@
 const {readFileSync} = require('fs');
 
+let challenge1: number[] = [];
+let challenge2: Array<number>  = [];
+
+class File {
+
+  name: string;
+  size: number;
+
+  constructor(name: string, size:number) {
+    this.name = name;
+    this.size = size;
+  }
+
+}
+
+class Dir {
+  parentDir: Dir;
+  name: string;
+  files: Map<string, File>;
+  dirs: Map<string, Dir>;
+
+
+
+  constructor(name: string, parentDir: Dir) {
+    this.parentDir = parentDir;
+    this.name = name;
+    this.files = new Map();
+    this.dirs = new Map();
+  }
+
+  AddFile(file: File) {
+    this.files.set(file.name, file);
+  }
+
+  AddDir(directory: Dir) {
+    this.dirs.set(directory.name, directory);
+  }
+
+  GetSize(): number {
+    let size = 0
+    // Get size of files
+    this.files.forEach((value: File, key: string) => {
+      size = size + value.size
+    });
+
+    // Get size of folders
+    if(this.dirs.size > 0) {
+      this.dirs.forEach((value: Dir, key: string) => {
+        size = size + value.GetSize()
+      });
+    }
+    if(size < 100000) {
+      challenge1.push(size);
+    }
+    challenge2.push(size);
+    return size;
+  }
+
+}
+
 export class Day7 {
+
+  currentDir: Dir;
 
   input: string;
 
@@ -10,14 +72,78 @@ export class Day7 {
     } else {
       this.input = input;
     }
+
+    this.ParseFileSystem();
   }
 
+
+  root = new Dir("/", null);
+
+  ParseFileSystem() {
+    let lines = this.input.split("\n");
+
+    for (var i = 0; i<lines.length; i++) {
+      let x = lines[i];
+      if (x === ""){
+
+      } else {
+        if(x.includes("$")) {
+          if (x.includes("cd")) {
+            var dirName = x.split(" ")[2];
+            if (dirName === "/") {
+              this.currentDir = this.root;
+            } else if (dirName === "..") {
+              this.currentDir = this.currentDir.parentDir;
+            } else {
+              this.currentDir = this.currentDir.dirs.get(dirName);
+            }
+          }
+          if (x.includes("ls")) {
+            while (i + 1 < lines.length && !lines[i + 1].includes("$")) {
+              i = i + 1;
+              var newLine = lines[i];
+              if(newLine === ""){
+
+              } else {
+                var split = newLine.split(" ")
+                if (split[0] == "dir") {
+                  this.currentDir.AddDir(new Dir(split[1], this.currentDir));
+                } else {
+                  this.currentDir.AddFile(new File(split[1], parseInt(split[0])));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+
   Challenge1():number {
-   return 0;
+    challenge1 = [];
+    let size = this.root.GetSize();
+
+    console.log(size);
+    let c1 = challenge1.reduce((partialSum, a) => partialSum + a, 0);
+
+    return c1
   }
 
   Challenge2():number {
-    return 0;
+    challenge2 = [];
+    let size = this.root.GetSize();
+    let freeSpace = 70000000 - size;
+
+    let needed = 30000000 - freeSpace;
+
+    console.log("Used size: " + size);
+    console.log("Needed size: " + needed);
+
+    let candidate = challenge2.filter( x => x >= needed)[0];
+    console.log(candidate);
+
+    return candidate;
   }
 
 }
--- test/jest/__tests__/day7.spec.ts
+++ test/jest/__tests__/day7.spec.ts
@@ -2,13 +2,37 @@ import {Day7} from "../../../src/Day7/Day7";
 
 describe('Day 7', () => {
 
+    const input = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`
+
     it('Challenge 1', () => {
-        let result = new Day7("").Challenge1();
-        expect(result).toBe(0);
+        let result = new Day7(input).Challenge1();
+        expect(result).toBe(95437);
     });
 
     it('Challenge 2', () => {
-        let result = new Day7("").Challenge2();
-        expect(result).toBe(0);
+        let result = new Day7(input).Challenge2();
+        expect(result).toBe(24933642);
     });
 });
